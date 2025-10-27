c9e2a807d5bc3b7faeb0ecd0e826eeb1de395935
Deivid021 <deividsestrensantos@gmail.com>
2025-10-26 22:48:46 -0300

    Aula 10 Execicio 01 finalizado

---
+++ php/Aula10/Exc01/acao.php
@@ -0,0 +1,29 @@
+<?php
+
+    session_start();
+    date_default_timezone_set('America/Sao_Paulo');
+
+    if(!isset($_SESSION['user'])){
+        header("Location: index.php");
+    }
+
+    $_SESSION['fim'] = date("d/m/y H:i:s");
+    $tempo_sessao = time() - $_SESSION['inicio_timestamp'];
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mostra sessão</title>
+</head>
+<body>
+    <h2>Informações da sessão</h2>
+    <p>Login: <?= $_SESSION['user']?></p>
+    <p>Senha: <?= $_SESSION['pass']?></p>
+    <p>Data e Hora do Inicio da Sessão: <?= $_SESSION['inicio']?></p>
+    <p>Data e Hora da Ultima Requisição: <?= $_SESSION['fim']?></p>
+    <p>Tempo de Sessão: <?= $tempo_sessao ?> segundos</p>
+</body>
+</html>
---
+++ php/Aula10/Exc01/index.php
@@ -0,0 +1,43 @@
+<?php
+    session_start();
+    date_default_timezone_set('America/Sao_Paulo');
+    
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $user = $_POST['usuario'];
+        $pass = $_POST['senha'];
+
+        if($user == 'adm' && $pass = '123'){
+            $_SESSION['user'] = $user;
+            $_SESSION['pass'] = $pass;
+            $_SESSION['inicio'] = date("d/m/y H:i:s");
+            $_SESSION['inicio_timestamp'] = time();
+            header("Location: acao.php");
+            exit;
+        } else {    
+            $erro = "Login ou Senha Incorretos!";
+        }
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <br>
+    <form action="" method="POST">
+        <label for="usuario">Usúario:</label>
+        <input type="text" name="usuario" id="usuario" required><br><br>
+        <label for="senha">Senha:</label>
+        <input type="password" name="senha" id="senha" required><br><br>
+
+        <input type="submit" value="Enviar">
+        <?php if(isset($erro)) echo "<p style='color:red;'>$erro</p>"; ?>
+    </form>
+</body>
+</html>
