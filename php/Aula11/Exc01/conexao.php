ed5865315a53608aa343ca13bb7b1209bf372e67
Deivid021 <deividsestrensantos@gmail.com>
2025-10-29 12:50:26 -0300

    conexao com banco de dados e inico das atividades

---
+++ php/Aula11/Exc01/conexao.php
@@ -0,0 +1,18 @@
+<?php
+
+function conecta(){
+    try {
+        $dbconn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=postgres");
+        if($dbconn) {
+            echo "database connect";
+        
+            $result = pg_query($dbconn, "SELECT COUNT(*) AS QTDTABS FROM PG_TABLES");
+        
+            // while($row = pg_fetch_assoc($result)) {
+            //     echo "<br> Result: ". $row['qtdtabs'];
+            // }
+        }
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
--- php/Aula11/Exc01/index.php
+++ php/Aula11/Exc01/index.php
@@ -1,15 +1,7 @@
 <?php
-$num1 = isset($_POST['num1']) ? $_POST['num1'] : 0; // breakpoint aqui
-$num2 = isset($_POST['num2']) ? $_POST['num2'] : 0; // breakpoint aqui
 
+require_once("conexao.php"); 
 
+conecta();
 
-
-function soma($num1, $num2){
-    $resultado = $num1 + $num2;
-
-    return $resultado;
-}
-
-
-echo "a soma é ". soma($num1, $num2); 
+$aDados = array()
